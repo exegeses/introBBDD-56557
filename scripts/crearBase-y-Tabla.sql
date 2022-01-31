3550ebbfe8fdbbc74f2eddaff6fcf80254d3f4f5
Marcos <exegeses@gmail.com>
2022-01-31 11:14:31 -0300

    scripts para crear una base y una tabla listo

--- scripts/crearBase-y-Tabla.sql
+++ scripts/crearBase-y-Tabla.sql
@@ -2,5 +2,20 @@
 CREATE DATABASE introDB2;
 
 -- Activar una base
+USE introDB2;
 
 -- Crear una tabla
+-- fórmula
+-- CREATE TABLE nombre
+-- (
+--     nCampo tipoDato características,
+--     nCampo tipoDato características,
+--     nCampo tipoDato características
+--)
+CREATE TABLE billeteras
+(
+    id int primary key auto_increment not null,
+    nombre varchar(50) not null,
+    precio float( 9, 2 ) not null,
+    stock int not null
+)
