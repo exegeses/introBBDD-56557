54df07fbb900530ecd185c7cab8039a2f81b12b6
Marcos <exegeses@gmail.com>
2022-02-03 11:50:48 -0300

    creación de tablas regiones y destinos

--- scripts/tabla-regiones.sql
+++ scripts/tabla-regiones.sql
@@ -1,5 +1,7 @@
 -- creación de tabla regiones
 CREATE TABLE regiones
     (
-        idRegion tinyint
+        idRegion tinyint primary key auto_increment not null,
+        regNombre varchar(75) not null
     )
+
