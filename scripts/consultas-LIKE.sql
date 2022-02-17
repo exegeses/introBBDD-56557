1f087f29cfbf82d66465029aef161404eb88b6eb
Marcos <exegeses@gmail.com>
2022-02-17 12:02:50 -0300

    Consultas usando LIKE

---
+++ scripts/consultas-LIKE.sql
@@ -0,0 +1,28 @@
+-- búsquedas por subcadena || que contenga
+
+-- que en le nombre contenga 'Marshall'
+SELECT prdNombre, prdDescripcion, prdPrecio
+    FROM productos
+    WHERE prdNombre LIKE '%Marshall%';
+
+-- que en la descripción contenga 'Bluetooth'
+SELECT prdNombre, prdDescripcion, prdPrecio
+    FROM productos
+    WHERE prdDescripcion LIKE '%Bluetooth%';
+
+-- ahora con más tablas
+-- nombre, precio, marca, descripción
+-- dónde en la descripción contenga 'Altavoz'
+
+
+SELECT prdNombre, prdPrecio,prdDescripcion, mkNombre
+    FROM productos
+     JOIN marcas
+      ON productos.idMarca=marcas.idmarca
+    WHERE prdDescripcion LIKE '%Altavoz%';
+
+SELECT prdNombre, prdDescripcion, mkNombre,prdPrecio
+    FROM productos, marcas
+    WHERE productos.idMarca = marcas.idMarca
+    AND prdDescripcion LIKE '%Altavoz%';
+
